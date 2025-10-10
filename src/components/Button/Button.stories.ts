df16f7faa4216fd3def6e099fa217465eaaaf1e3
Lyaguf <kovtunov_10@mail.ru>
2025-10-10 19:33:29 +0300

    feat: add tmplt and button

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,27 @@
+import ButtonComponent from './Button.vue';
+
+export default {
+  title: 'Example/Button',
+  component: ButtonComponent, 
+  argTypes: {
+    variant: { control: { type: 'select' }, options: ['filled', 'stroke', 'dark-filled'] },
+    as: { control: { type: 'text' } },
+  },
+  args: {
+    as: 'button',
+    variant: 'filled',
+    disabled: false,
+  },
+};
+
+// Базовая стори для документации
+export const Button = {
+  render: (args: any) => ({
+    components: { ButtonComponent },
+    setup() {
+      return { args };
+    },
+    template: '<ButtonComponent v-bind="args">Button</Button>',
+  }),
+};
+
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+import './components/style/variables.css';
